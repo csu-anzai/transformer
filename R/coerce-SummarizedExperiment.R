2a5bce959fabba511a429b14f442c996ece0c835
Michael Steinbaugh <mike@steinbaugh.com>
2019-01-06 16:18:33 -0500

    Ensure `as.SummarizedExperiment()` is exported, so we can use in basejump

--- R/coerce-SummarizedExperiment.R
+++ R/coerce-SummarizedExperiment.R
@@ -1,3 +1,25 @@
+# FIXME Need to export this to access in basejump...
+
+
+
+#' Coerce to `SummarizedExperiment`
+#'
+#' Improved method for reliably coercing objects that extend
+#' `RangedSummarizedExperiment` to a standard `SummarizedExperiment`, that
+#' doesn't drop [`rowData()`][SummarizedExperiment::rowData].
+#'
+#' @name coerce-SummarizedExperiment
+#' @examples
+#' ## RangedSummarizedExperiment ====
+#' load(system.file("extdata", "rse.rda", package = "S4Transformer"))
+#' x <- as.SummarizedExperiment(rse)
+NULL
+
+
+
+# S3 ===========================================================================
+#' @rdname coerce-SummarizedExperiment
+#' @export
 as.SummarizedExperiment <-  # nolint
     function(x) {
         UseMethod("as.SummarizedExperiment")
@@ -8,6 +30,8 @@ as.SummarizedExperiment <-  # nolint
 # Note that our method here keeps track of `rowData` when coercing an object
 # that extends RangedSummarizedExperiment to SummarizedExperiment. This bug
 # needs to be fixed in the SummarizedExperiment package.
+#' @method as.SummarizedExperiment default
+#' @export
 as.SummarizedExperiment.default <-  # nolint
     function(x) {
         if (is(x, "RangedSummarizedExperiment")) {
