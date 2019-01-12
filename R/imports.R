5dfda139fa15c4fd8e9d464a9aec88bb618ed5a4
Michael Steinbaugh <mike@steinbaugh.com>
2019-01-12 07:56:40 -0500

    Rename package to simply "transformer", from "S4Transformer"

---
+++ R/imports.R
@@ -0,0 +1,20 @@
+#' @importClassesFrom GenomicRanges GRanges
+#' @importClassesFrom Matrix sparseMatrix
+#' @importClassesFrom S4Vectors DataFrame
+#' @importClassesFrom SummarizedExperiment SummarizedExperiment
+#' @importClassesFrom tibble tbl_df
+#'
+#' @importFrom BiocGenerics as.data.frame
+#' @importFrom S4Vectors metadata metadata<-
+#' @importFrom SummarizedExperiment rowData rowData<- rowRanges
+#' @importFrom goalie assert hasLength hasRownames
+#' @importFrom methods as is setAs setMethod signature slotNames .hasSlot
+#' @importFrom tibble as_tibble
+NULL
+
+
+
+# This is needed to properly declare S4 `as()` coercion methods.
+#' @importFrom methods coerce
+#' @exportMethod coerce
+NULL
