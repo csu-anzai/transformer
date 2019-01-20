7d07540eae01f6a3b0b67e1592c8b37466e07a5c
Michael Steinbaugh <mike@steinbaugh.com>
2019-01-20 14:55:18 -0500

    Prepare unit tests

--- tests/testthat/test_coerceS4ToList.R
+++ tests/testthat/test_coerceS4ToList.R
@@ -2,20 +2,21 @@ context("Coerce S4 to list")
 
 load(system.file("extdata", "rse.rda", package = "transformer"))
 
-
-
-test_that("coerceS4ToList", {
-    object <- coerceS4ToList(rse)
-    expect_is(object, "list")
-    expect_identical(
-        object = names(object),
-        expected = c(
-            "rowRanges",
-            "colData",
-            "assays",
-            "NAMES",
-            "elementMetadata",
-            "metadata"
+with_parameters_test_that(
+    "Coerce S4 to list", {
+        object <- fun(rse)
+        expect_is(object, "list")
+        expect_identical(
+            object = names(object),
+            expected = c(
+                "rowRanges",
+                "colData",
+                "assays",
+                "NAMES",
+                "elementMetadata",
+                "metadata"
+            )
         )
-    )
-})
+    },
+    fun = list(coerceS4ToList, flatFiles)
+)
