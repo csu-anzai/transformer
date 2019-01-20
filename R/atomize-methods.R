08a0d4cfd264d259e1f25a6a90618e989f3b87c0
Michael Steinbaugh <mike@steinbaugh.com>
2019-01-20 06:54:19 -0500

    Improve handling of Rle columns for BioC 3.7

--- R/atomize-methods.R
+++ R/atomize-methods.R
@@ -16,13 +16,19 @@ bioverbs::atomize
 
 
 
+# @seealso
+# - `base:::as.data.frame.matrix()`.
+# - `S4Vectors:::as.data.frame.DataTable()`.
 atomize.ANY <-  # nolint
     function(object) {
         class <- class(object)[[1L]]
-        # First, coerce to S3 data frame.
-        # This step helps coerce nested S4 data to atomic columns.
-        # This will also decode Rle columns.
-        df <- as.data.frame(object)
+        # Note that going straight to data.frame using `as.data.frame()` doesn't
+        # handle stringsAsFactors correctly for Rle, at least in BioC 3.7.
+        df <- as(object, "DataFrame")
+        # This step will convert Rle columns (e.g. in GRanges mcols).
+        df <- decode(df)
+        # Including this step here to coerce complex S4 columns, like IRanges.
+        df <- as.data.frame(df)
         # Keep only atomic columns. Complex columns won't write to disk as CSVs
         # or work with R Markdown functions.
         keep <- vapply(X = df, FUN = is.atomic, FUN.VALUE = logical(1L))
