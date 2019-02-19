3090608b19f2c9caf895adb79179615b15622b4c
Michael Steinbaugh <mike@steinbaugh.com>
2019-02-18 19:14:11 -0500

    Add data.table code coverage

--- tests/testthat/test_coercion_methods.R
+++ tests/testthat/test_coercion_methods.R
@@ -6,6 +6,7 @@ load(system.file("extdata", "tbl_df.rda", package = "transformer"))
 
 DataFrame <- S4Vectors::DataFrame
 colData <- SummarizedExperiment::colData
+data.table <- data.table::data.table  # nolint
 rowData <- SummarizedExperiment::rowData
 rowRanges <- SummarizedExperiment::rowRanges
 tibble <- tibble::tibble
@@ -26,6 +27,58 @@ with_parameters_test_that(
         tbl_df
     )
 )
+
+
+
+# data.table ===================================================================
+test_that("data.frame to data.table", {
+    data <- data.frame()
+    data <- as(data, "data.table")
+    expect_is(data, "data.table")
+})
+
+test_that("DataFrame to data.table", {
+    data <- colData(rse)
+    x <- as(data, "data.table")
+    expect_is(x, "data.table")
+
+    # Expect that rownames are automatically moved to first column.
+    expect_identical(colnames(x)[[1L]], "rowname")
+
+    # Early return if already data.table.
+    x <- data.table()
+    expect_identical(x, as(x, "data.table"))
+
+    # Coercion of a DataFrame containing a list column is allowed.
+    data <- DataFrame()
+    data$x <- list()
+    data <- as(data, "data.table")
+    expect_is(data, "data.table")
+
+    # Error on complex S4 column (e.g. GRanges).
+    data <- as(rowRanges(rse), "DataFrame")
+    expect_s4_class(data[["X"]], "GRanges")
+    # Expect error on "X" column, which contains nested GRanges.
+    expect_error(
+        object = as(data, "data.table"),
+        regexp = "X"
+    )
+
+    # Check handling when rownames are NULL.
+    data <- DataFrame(a = 1L, b = "b")
+    expect_null(rownames(data))
+    data <- as(data, "data.table")
+    expect_is(data, "data.table")
+    # Note that tibble doesn't support row names, but they still return like
+    # standard data.frame class, where you can't actually set NULL.
+    expect_identical(rownames(data), "1")
+})
+
+test_that("GRanges to data.table", {
+    data <- rowRanges(rse)
+    data <- as(data, "data.table")
+    expect_is(data, "data.table")
+})
 
 
 
@@ -114,7 +167,7 @@ test_that("DataFrame to tbl_df", {
     )
 
     # Check handling when rownames are NULL.
-    data <- DataFrame(a = 1, b = "b")
+    data <- DataFrame(a = 1L, b = "b")
     expect_null(rownames(data))
     data <- as(data, "tbl_df")
     expect_is(data, "tbl_df")
