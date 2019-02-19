ec5784d307c9c75ebbdacbb20671730c5dc95ca3
Michael Steinbaugh <mike@steinbaugh.com>
2019-02-18 19:07:54 -0500

    Save internal `.coerceDataFrame()` utility

---
+++ R/data.frame-internal.R
@@ -0,0 +1,32 @@
+# Coerce an S4 DataFrame to a standard data.frame.
+# This functional will return an informative error if an S4 DataFrame contains
+# complex columns that can't be coerced to atomic or list.
+.coerceDataFrame <- function(x) {
+    # Check for valid columns (atomic, list).
+    valid <- vapply(
+        X = x,
+        FUN = function(x) {
+            is.atomic(x) || is.list(x)
+        },
+        FUN.VALUE = logical(1L),
+        USE.NAMES = TRUE
+    )
+    # Error if S4 columns are nested.
+    if (!all(valid)) {
+        invalid <- x[, names(valid[!valid]), drop = FALSE]
+        invalid <- vapply(
+            X = invalid,
+            FUN = class,
+            FUN.VALUE = character(1)
+        )
+        stop(paste(
+            "Only atomic and list columns are supported.",
+            "Invalid columns:",
+            printString(invalid),
+            sep = "\n"
+        ))
+    }
+    # Don't use `as.data.frame()` here. It can unexpectedly sanitize row
+    # names (e.g. gene symbols), whereas the `as()` method does not.
+    as(x, "data.frame")
+}
