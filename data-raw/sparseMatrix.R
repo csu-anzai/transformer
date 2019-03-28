cbdd6194feeb1f4c6fe08bc9982986185bf6c943
Michael Steinbaugh <mike@steinbaugh.com>
2019-03-28 09:11:23 -0400

    Migrate these legacy example scripts to acidtest

--- data-raw/sparseMatrix.R
+++
@@ -1,6 +0,0 @@
-# sparseMatrix
-i <- c(1, 3:8)
-j <- c(2, 9, 6:10)
-x <- 7 * (1:7)
-sparseMatrix <- Matrix::sparseMatrix(i = i, j = j, x = x)
-save(sparseMatrix, file = file.path("inst", "extdata", "sparseMatrix.rda"))
--- data-raw/tbl_df.R
+++
@@ -1,2 +0,0 @@
-tbl_df <- head(tibble::as_tibble(datasets::iris))
-save(tbl_df, file = file.path("inst", "extdata", "tbl_df.rda"))
