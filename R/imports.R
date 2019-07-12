a0a49373657d2e03e6bb6a8803bfa3d9b51b981e
Michael Steinbaugh <mike@steinbaugh.com>
2019-07-12 15:08:24 -0400

    Update NAMESPACE

--- R/imports.R
+++ R/imports.R
@@ -8,8 +8,8 @@
 #' @importFrom IRanges end start width
 #' @importFrom S4Vectors as.data.frame mcols merge metadata metadata<-
 #' @importFrom SummarizedExperiment rowData rowData<- rowRanges
-#' @importFrom goalie areDisjointSets assert hasLength hasRownames isCharacter
-#'   printString
+#' @importFrom goalie areDisjointSets assert hasColnames hasLength hasRownames
+#'   isCharacter printString validNames
 #' @importFrom methods as is setAs setMethod signature slotNames .hasSlot
 #' @importFrom tibble as_tibble
 NULL
