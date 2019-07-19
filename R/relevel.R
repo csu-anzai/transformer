4887ee28f0fb63a914bd64c558306bec534cf74c
Michael Steinbaugh <mike@steinbaugh.com>
2019-07-19 12:09:07 -0400

    Improve documentation

--- R/relevel.R
+++ R/relevel.R
@@ -2,7 +2,7 @@
 #'
 #' @name relevel
 #' @inheritParams params
-#' @param ... Additional arguments
+#' @param ... Additional arguments.
 #'
 #' @param ref `NULL`.
 #'   Unsupported argument that is part of S3 generic.
