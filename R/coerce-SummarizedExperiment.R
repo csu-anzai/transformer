f71e8c64cbc3c0f4dcd3639948ac6749f97bf87c
Michael Steinbaugh <mike@steinbaugh.com>
2019-07-19 10:56:36 -0400

    Improve documentation regarding current S4 coercion problems

--- R/coerce-SummarizedExperiment.R
+++ R/coerce-SummarizedExperiment.R
@@ -1,6 +1,8 @@
-#' Coerce to `SummarizedExperiment`
+#' Coerce to summarized experiment
 #'
-#' Improved method for reliably coercing objects that extend
+#' Coerce to `SummarizedExperiment`.
+#'
+#' Improved S3 methods for reliably coercing objects that extend
 #' `RangedSummarizedExperiment` to a standard `SummarizedExperiment`, that
 #' doesn't drop [`rowData()`][SummarizedExperiment::rowData].
 #'
@@ -9,11 +11,25 @@
 #' @inheritParams params
 #'
 #' @examples
-#' data(rse, package = "acidtest")
+#' data(rse, sce, package = "acidtest")
 #'
-#' ## SummarizedExperiment ====
-#' x <- as.SummarizedExperiment(rse)
-#' x
+#' ## RangedSummarizedExperiment ===
+#' ## Current S4 coercion drops row metadata.
+#' names(metadata(rowRanges(rse)))
+#' se <- as(rse, "SummarizedExperiment")
+#' names(metadata(rowData(se)))
+#' ## Our alternate S3 method preserves the metadata.
+#' se <- as.SummarizedExperiment(rse)
+#' names(metadata(rowData(se)))
+#'
+#' ## Extends RangedSummarizedExperiment ====
+#' is(sce, "RangedSummarizedExperiment")
+#' ## Current S4 coercion drops row data.
+#' se <- as(sce, "SummarizedExperiment")
+#' rowData(se)
+#' ## Our alternate S3 method preserves the row data.
+#' se <- as.SummarizedExperiment(sce)
+#' rowData(se)
 NULL
 
 
@@ -28,26 +44,66 @@ as.SummarizedExperiment <-  # nolint
 
 
 
-# Note that our method here keeps track of `rowData()` when coercing an object
-# that extends RangedSummarizedExperiment to SummarizedExperiment. This bug
-# needs to be fixed in SummarizedExperiment.
-
-#' @method as.SummarizedExperiment default
+#' @rdname coerce-SummarizedExperiment
 #' @export
-as.SummarizedExperiment.default <-  # nolint
+as.SummarizedExperiment.RangedSummarizedExperiment <-  # nolint
     function(x) {
-        if (is(x, "RangedSummarizedExperiment")) {
-            rowMeta <- metadata(rowRanges(x))
-            x <- as(x, "RangedSummarizedExperiment")
-        } else {
-            rowMeta <- metadata(rowData(x))
-        }
-        x <- as(x, "SummarizedExperiment")
-        metadata(rowData(x)) <- rowMeta
-        x
+        `.coerce.RangedSummarizedExperiment,SummarizedExperiment`(x)
     }
 
 
 
 # S4 ===========================================================================
-# S4 methods are already defined in SummarizedExperiment.
+# Coercion methods of interest:
+# - from="RangedSummarizedExperiment", to="SummarizedExperiment"
+# - from="SingleCellExperiment", to="RangedSummarizedExperiment"
+# - from="SingleCellExperiment", to="SummarizedExperiment"
+
+
+
+# Updated 2019-07-19.
+`.coerce.RangedSummarizedExperiment,SummarizedExperiment` <-  # nolint
+    function(from) {
+        # Keep track of row metadata.
+        rowMeta <- metadata(rowRanges(from))
+
+        # Default coercion method in SummarizedExperiment package.
+        method <- getMethod(
+            f = "coerce",
+            signature = signature(
+                from = "RangedSummarizedExperiment",
+                to = "SummarizedExperiment"
+            ),
+            where = asNamespace("SummarizedExperiment")
+        )
+        to <- method(from)
+
+        # Reslot the row metadata.
+        metadata(rowData(to)) <- rowMeta
+
+        to
+    }
+
+
+
+# nolint start
+
+# Can't set this as S4 coercion method because RSE and SE aren't local,
+# modifiable classes in the package.
+#
+# If you attempt to define `coerce` method with `setAs()`, this will error:
+#
+# > Error in setIs(from, to, test = test, coerce = def, replace = replace,  :
+# >   cannot create a 'setIs' relation when neither of the classes
+# >   (“RangedSummarizedExperiment” and “SummarizedExperiment”) is local and
+# >   modifiable in this package
+
+# #' @rdname coerce-SummarizedExperiment
+# #' @name coerce,RangedSummarizedExperiment,SummarizedExperiment-method
+# setAs(
+#     from = "RangedSummarizedExperiment",
+#     to = "SummarizedExperiment",
+#     def = `.coerce.RangedSummarizedExperiment,SummarizedExperiment`
+# )
+
+# nolint end
