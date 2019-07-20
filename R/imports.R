3f86b97aad93ef8f4d55f771b86cf72e3393d72e
Michael Steinbaugh <mike@steinbaugh.com>
2019-07-20 08:57:55 -0400

    Update NAMESPACE

--- R/imports.R
+++ R/imports.R
@@ -1,7 +1,7 @@
-#' @importClassesFrom GenomicRanges GRanges
-#' @importClassesFrom IRanges IRanges
-#' @importClassesFrom Matrix sparseMatrix
-#' @importClassesFrom S4Vectors DataFrame
+#' @importClassesFrom GenomicRanges GenomicRanges
+#' @importClassesFrom IRanges IPosRanges Ranges
+#' @importClassesFrom Matrix Matrix
+#' @importClassesFrom S4Vectors DataFrame Vector
 #' @importClassesFrom SummarizedExperiment RangedSummarizedExperiment
 #'   SummarizedExperiment
 #' @importClassesFrom tibble tbl_df
@@ -10,8 +10,8 @@
 #' @importFrom S4Vectors DataFrame Rle as.data.frame decode mcols mcols<- merge
 #'   metadata metadata<-
 #' @importFrom SummarizedExperiment rowData rowData<- rowRanges
-#' @importFrom goalie areDisjointSets assert hasLength hasRownames isCharacter
-#'   printString validNames
+#' @importFrom goalie areDisjointSets assert hasLength hasRows hasRownames
+#'   isCharacter printString validNames
 #' @importFrom methods as getMethod is setAs setMethod signature slotNames
 #'   .hasSlot
 #' @importFrom stats na.omit
