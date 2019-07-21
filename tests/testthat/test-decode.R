74484241203f46088364244fa2a8b2fa9c54f4f6
Michael Steinbaugh <mike@steinbaugh.com>
2019-07-21 09:35:35 -0400

    Add SE coverage

--- tests/testthat/test-decode.R
+++ tests/testthat/test-decode.R
@@ -2,14 +2,23 @@ context("decode")
 
 test_that("DataFrame", {
     x <- encode(df)
+    expect_s4_class(x[[1L]], "Rle")
     y <- decode(x)
     expect_is(y[[1L]], "integer")
 })
 
 test_that("GRanges", {
     x <- encode(gr)
+    expect_s4_class(mcols(x)[[1L]], "Rle")
     y <- decode(x)
-    expect_is(mcols(y)[[1L]], "character")
+    expect_type(mcols(y)[[1L]], "character")
 })
 
-## FIXME Add coverage for SummarizedExperiment
+test_that("SummarizedExperiment", {
+    x <- encode(rse)
+    expect_s4_class(rowData(x)[[1L]], "Rle")
+    expect_s4_class(colData(x)[[1L]], "Rle")
+    y <- decode(rse)
+    expect_type(rowData(y)[[1L]], "character")
+    expect_s3_class(colData(y)[[1L]], "factor")
+})
