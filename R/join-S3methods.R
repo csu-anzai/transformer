5f58714162ba5d09fef310103381c4c905f766c7
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-07 23:39:01 -0400

    Improve join generic reexport from dplyr

--- R/join-S3methods.R
+++ R/join-S3methods.R
@@ -48,24 +48,17 @@ NULL
 ## Can consider using Hervé Pagès's recommended approach instead.
 ## https://support.bioconductor.org/p/120277/
 
-## Consider importing dplyr and reexporting the S3 generics defined there
-## instead. Note that those contain additional arguments, such as "copy" and
-## "suffix".
+
+
+#' @importFrom dplyr left_join
+#' @export
+dplyr::left_join
 
 
 
 #' @rdname join
 #' @export
-left_join <-  # nolint
-    function(x, y, by) {
-        UseMethod("left_join")
-    }
-
-
-
-#' @rdname join
-#' @export
-## Updated 2019-07-19.
+## Updated 2019-08-07.
 left_join.DataFrame <-  # nolint
     function(x, y, by) {
         assert(
