3cfbcbae822ed640002fc7abe26e22bf9260392e
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-26 18:16:06 -0400

    Improve code coverage

--- tests/testthat/test-mutate.R
+++ tests/testthat/test-mutate.R
@@ -3,31 +3,31 @@ context("mutate")
 x <- mtcars
 
 test_that("mutateAll", {
-    x <- mutateAll(x, .fun = log, base = 2L)
+    x <- mutateAll(x, fun = log, base = 2L)
     expect_s4_class(x, "DataFrame")
     expect_true(hasRownames(x))
 })
 
 test_that("mutateAt", {
-    x <- mutateAt(x, .vars = c("mpg", "cyl"), log, base = 2L)
+    x <- mutateAt(x, vars = c("mpg", "cyl"), log, base = 2L)
     expect_s4_class(x, "DataFrame")
     expect_true(hasRownames(x))
 })
 
 test_that("mutateIf", {
-    x <- mutateIf(x, .predicate = is.double, .fun = as.integer)
+    x <- mutateIf(x, predicate = is.double, fun = as.integer)
     expect_s4_class(x, "DataFrame")
     expect_true(hasRownames(x))
 })
 
 test_that("transmuteAt", {
-    x <- transmuteAt(x, .vars = c("mpg", "cyl"), log, base = 2L)
+    x <- transmuteAt(x, vars = c("mpg", "cyl"), log, base = 2L)
     expect_s4_class(x, "DataFrame")
     expect_true(hasRownames(x))
 })
 
 test_that("transmuteIf", {
-    x <- transmuteIf(x, .predicate = is.double, .fun = as.integer)
+    x <- transmuteIf(x, predicate = is.double, fun = as.integer)
     expect_s4_class(x, "DataFrame")
     expect_true(hasRownames(x))
 })
--- tests/testthat/test-select.R
+++ tests/testthat/test-select.R
@@ -3,7 +3,7 @@ context("select")
 x <- mtcars
 
 test_that("selectIf", {
-    x <- selectIf(x, .predicate = is.double)
+    x <- selectIf(x, predicate = is.double)
     expect_s4_class(x, "DataFrame")
     expect_true(hasRownames(x))
 })
